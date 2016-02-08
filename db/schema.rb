3238b58522ee5ecdbe13633ac3b6207f38f99a6e
Doug Wright <mrdougwright@gmail.com>
2016-02-08 03:14:05 -0800

    add empty test files

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160208110937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "actors", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "movie_actors", force: :cascade do |t|
+    t.integer  "actor_id"
+    t.integer  "movie_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "movie_actors", ["actor_id"], name: "index_movie_actors_on_actor_id", using: :btree
+  add_index "movie_actors", ["movie_id"], name: "index_movie_actors_on_movie_id", using: :btree
+
+  create_table "movies", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "year"
+    t.datetime "release_date"
+    t.integer  "critics_score"
+    t.string   "rotten_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
 end
