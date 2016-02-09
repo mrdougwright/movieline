7b8557be9794e1116d118f35b3cba118484eb030
Doug Wright <mrdougwright@gmail.com>
2016-02-09 02:17:01 -0800

    fix bug with find_create_movies

--- lib/find/find_create_movies.rb
+++ lib/find/find_create_movies.rb
@@ -4,15 +4,21 @@ module FindCreateMovies
     tmdb_movies = Tmdb::Person.credits(tmdb_id)['cast'].select{|m| m['media_type'] == 'movie'}
 
     tmdb_movies.each do |movie|
-      unless actor.movies.pluck(:title).include? movie['title']
+      if Movie.pluck(:title).include? movie['title']
+        m = Movie.find_by_title(movie['title'])
+      else
         movie_detail = Tmdb::Movie.detail(movie['id'])
         m = Movie.create(
           title: movie['title'], release_date: movie['release_date'],
           tmdb_id: movie['id'], vote_average: movie_detail['vote_average']
         )
+      end
+
+      if MovieActor.where(actor_id: actor.id, movie_id: m.id).blank?
         MovieActor.create(actor_id: actor.id, movie_id: m.id)
       end
     end
+
     return actor.movies
   end
 
